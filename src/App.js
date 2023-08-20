fd26a21533a237b45c853d1be9f819a55f667c73
larastrokach <larastrokach@gmail.com>
2023-08-20 23:09:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { data } from './data';
+import  image  from './test.jpeg';
+import { useState } from "react";
 import './App.css';
-
+ 
 function App() {
+  const [tests, setTests] = useState (data);
+  const [showText, setShowText] = useState (false)
+  
+  const removeTest = (id) =>{
+
+    let newTests = tests.filter(test=> test.id !== id )
+    setTests(newTests)
+  }
+
+  const showTextClick  = (element) => {
+    element.showMore = !element.showMore
+    setShowText(!showText)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className= "">
+   <h1>List of {tests.length} tests every woman needs in her 40s</h1>
+   </div>
+   <div  className= "image">
+        <img src = {image} width="500px" alt ="women"/>
+      </div>
+
+   {tests.map((element => {
+    const {id, test, description, showMore} = element;
+
+    return(
+      <div className= "box" key={id}>
+      <div className= "container">
+       <h2>{id}. {test}</h2>
+       </div>
+       
+       <div className= "container">
+       <h3>{ showMore ? description : description.substring (0,200) + "..."}
+       <button className= "show" onClick= {() => showTextClick (element)} > {showMore ? "Show less" : "Show more"}</button>
+       </h3>
+       </div>
+
+      <div className= "container">
+      <button className= "btn" onClick={() => removeTest (id)}>Remove test</button>
+      </div>
+      </div> )
+   }))}
+
+<div className= "container">
+   <button className= "btn"onClick={() => setTests([])}>Delete all</button>
     </div>
-  );
+  </div>
+
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const data = [
+
+  {
+    id: 1,
+    test: "Body mass index (BMI)/obesity screening",
+    description: "Your weight and height are measured to calculate your BMI. If your BMI is 25 to 30 percent, you’re considered overweight. Anything more than 30 percent is obese. The higher your BMI, the more chance you have of developing heart disease, high blood pressure, type 2 diabetes, certain cancers and other diseases. Losing about 5 to 10 percent of your body weight can help reduce the risk of developing illnesses associated with obesity."
+  },
+
+
+  {
+    id: 2,
+    test: "Depression screening",
+    description: "You'll answer a series of questions to determine if you’re depressed or at risk for suicide. Your doctor will assess your situation and, if necessary, recommend treatment that could include counseling and/or medication."
+  },
+
+  {
+    id: 3,
+    test: "Alcohol, smoking and substance misuse screening",
+    description: "You'll fill out a questionnaire to determine amounts and frequency of use. If your answers raise concerns, the doctor will follow with questions to determine if there’s a dependence. If you’re using tobacco or other potentially harmful drugs, your doctor will discuss quitting and a plan to achieve it."
+  },
+  
+  {
+    id: 4,
+    test: "Human immunodeficiency virus (HIV) screening",
+    description: "Everyone should be tested for HIV (through a blood test) regardless of sexual activity or lifestyle. HIV can be transmitted through body fluids like blood, semen and breast milk, or by sharing needles and blood transfusions."
+  },
+
+  {
+    id: 5,
+    test: "Blood pressure check",
+    description: "Your doctor will measure blood pressure with a cuff. The result is two numbers: top (systolic) and bottom (diastolic). For a healthy person, a normal range is under 130 on the top and under 80 on the bottom, guidelines that were lowered in late 2017. If you have other medical problems or diseases in your family, your blood pressure goal may be lower than 130/80. If you’re at risk for developing high blood pressure, your doctor can recommend diet and exercise plans to reduce your risk."
+  },
+
+  {
+    id: 6,
+    test: "Cholesterol screening",
+    description: "A cholesterol screening (also called a lipid panel) is a blood test that measures the amount of cholesterol and triglycerides in your blood. Those numbers help determine if you’re at risk for cardiovascular disease. Your body needs some cholesterol. But if you have too much, it can build up in your arteries and put you at risk of stroke and heart attack. Your doctor will recommend lifestyle changes if you’re at risk."
+  },
+  
+  {
+    id: 7,
+    test: "Diabetes screening as part of a cardiovascular risk assessment",
+    description: "You’ll take the fasting plasma glucose test, which involves checking the glucose level in your blood while you are fasting. If your blood glucose level is too high, you will be retested later to confirm a diabetes diagnosis. If there’s a problem, a doctor will recommend lifestyle changes."
+  },
+  
+  {
+    id: 8,
+    test: "Mammogram screening for breast cancer",
+    description: "Women should begin mammogram tests (it’s an X-ray of the breast) at age 40 to establish a baseline, then every year."
+  },
+  
+  {
+    id: 9,
+    test: "Pap test screening for cervical cancer",
+    description: "Women also get a Pap test, which looks for any abnormal cells on the cervix, as well as for HPV (human papillomavirus) infection. Women should have their first Pap test at age 21, and be retested every three to five years."
+  },
+  
+  {
+    id: 10,
+    test: "Eye and dental exams",
+    description: "If you wear glasses or contact lenses, keep up with the changes in your vision by updating your prescription. But you also want to be certain there are no vision problems developing. You should have eye exams at least every two years once you’ve reached 40. Schedule dental cleanings and exams every six months, and be diligent about brushing and flossing."
+  },
+
+  {
+    id: 11,
+    test: "Influenza vaccine",
+    description: "Get an annual flu vaccine before flu begins spreading in your community. It takes about two weeks after vaccination for antibodies that protect against flu to develop in your body. So aim to get your flu shot by the end of October."
+  },
+  
+  {
+    id: 12,
+    test: "Tetanus-diphtheria booster",
+    description: "You likely received tetanus and diphtheria vaccines as an infant or youth. A booster is recommended every 10 years. You’ll receive the shot in your arm."
+  }
+  
+  
+]
